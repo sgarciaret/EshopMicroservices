9236bc00d48fde23f42ee67c06e9df25dc59aceb
Sergio Garcia Retamero <Sergio.GarciaR@solera.com>
2024-12-09 15:16:09 +0100

    End of section 8

---
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Basket.API.Basket.DeleteBasket
+{
+    public record DeleteBasketCommand(string userName) : ICommand<DeleteBasketResult>;
+    public record DeleteBasketResult(bool IsSuccess);
+
+    public class DeleteBasketCommandValidator : AbstractValidator<DeleteBasketCommand>
+    {
+        public DeleteBasketCommandValidator()
+        {
+            RuleFor(x => x.userName).NotEmpty().WithMessage("UserName is required");
+        }
+    }
+
+    public class DeleteBasketCommandHandler(IBasketRepository repository) : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
+    {
+        public async Task<DeleteBasketResult> Handle(DeleteBasketCommand command, CancellationToken cancellationToken)
+        {
+            await repository.DeleteBasket(command.userName, cancellationToken);
+
+            return new DeleteBasketResult(true);
+        }
+    }
+}
