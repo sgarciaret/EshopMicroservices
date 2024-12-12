7c8c0688c1d3e1ae747c76464518e3e3892cb466
Sergio Garcia Retamero <Sergio.GarciaR@solera.com>
2024-12-12 11:17:54 +0100

    End of section 14

---
+++ src/Services/Ordering/Ordering.Domain/ValueObjets/Payment.cs
@@ -0,0 +1,32 @@
+﻿namespace Ordering.Domain.ValueObjets
+{
+    public record Payment
+    {
+        public string? CardName { get; } = default!;
+        public string CardNumber { get; } = default!;
+        public string Expiration { get; } = default!;
+        public string CVV { get; } = default!;
+        public int PaymentMethod { get; } = default!;
+
+        protected Payment() { }
+
+        private Payment(string cardName, string cardNumber, string expiration, string cVV, int paymentMethod)
+        {
+            CardName = cardName;
+            CardNumber = cardNumber;
+            Expiration = expiration;
+            CVV = cVV;
+            PaymentMethod = paymentMethod;
+        }
+
+        public static Payment Of(string cardName, string cardNumber, string expiration, string cVV, int paymentMethod)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(cardName);
+            ArgumentException.ThrowIfNullOrWhiteSpace(cardNumber);
+            ArgumentException.ThrowIfNullOrWhiteSpace(cVV);
+            ArgumentOutOfRangeException.ThrowIfGreaterThan(cVV.Length, 3);
+
+            return new Payment(cardName, cardNumber, expiration, cVV, paymentMethod);
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Domain/ValueObjets/ProductId.cs
@@ -0,0 +1,22 @@
+﻿using MediatR.NotificationPublishers;
+
+namespace Ordering.Domain.ValueObjets
+{
+    public record ProductId
+    {
+        public Guid Value { get; }
+
+        private ProductId(Guid value) => Value = value;
+
+        public static ProductId Of(Guid value)
+        {
+            ArgumentNullException.ThrowIfNull(value);
+            if (value == Guid.Empty)
+            {
+                throw new DomainException("ProductId cannot be empty.");
+            }
+
+            return new ProductId(value);
+        }
+    }
+}
